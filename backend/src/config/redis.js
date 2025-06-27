93778384578817eaaab804e2643ed2e88b37d352
garvshas <garvshas@gmail.com>
2025-06-27 09:07:52 +0530

    Add files via upload

---
+++ backend/src/config/redis.js
@@ -0,0 +1,13 @@
+const { createClient }  = require('redis');
+
+const redisClient = createClient({
+    username: 'default',
+    password: process.env.REDIS_PASS,
+    socket: {
+        host: 'redis-18473.c264.ap-south-1-1.ec2.redns.redis-cloud.com',
+        port: 18473
+    }
+});
+
+module.exports = redisClient;
+
